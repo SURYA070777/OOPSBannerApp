1e96f45387df98f718f3ae7656f500989a22b371
SURYA070777 <sv1167@srmist.edu.in>
2026-02-25 06:27:57 +0530

    Error

--- SRC/PrintOops.java
+++ SRC/PrintOops.java
@@ -1,5 +1,17 @@
 public class PrintOops {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("OOPS");
     }
 }
+=======
+        System.out.println(String.join(" ","   ****   ","   ****    ","  *******  ","   ***** "));
+        System.out.println(String.join(" ","  **  **  ","  **  **   ","  **    ** "," **    "));
+        System.out.println(String.join(" "," **    ** "," **    **  ","  **    ** ","  **    "));
+        System.out.println(String.join(" "," **    ** "," **    **  ","  *******  ","    *** "));
+        System.out.println(String.join(" "," **    ** "," **    **  ","  **       ","       **"));
+        System.out.println(String.join(" ","  **  **  ","  **  **   ","  **       ","       **"));
+        System.out.println(String.join(" ","   ****   ","   ****    ","  **       ","   ***** "));
+    }
+}
+>>>>>>> feature/UC3-StringJoin
