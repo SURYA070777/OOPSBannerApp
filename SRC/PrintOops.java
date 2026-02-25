86ef03ea286b087e9dd8976564d2d760afb2cb1c
SURYA070777 <sv1167@srmist.edu.in>
2026-02-25 06:23:37 +0530

    Error

--- SRC/PrintOops.java
+++ SRC/PrintOops.java
@@ -1,11 +1,5 @@
 public class PrintOops {
     public static void main(String[] args) {
-        System.out.println("   ****      ****      *******     ***** ");
-        System.out.println("  **  **    **  **     **    **   **    ");
-        System.out.println(" **    **  **    **    **    **   **    ");
-        System.out.println(" **    **  **    **    *******      *** ");
-        System.out.println(" **    **  **    **    **              **");
-        System.out.println("  **  **    **  **     **              **");
-        System.out.println("   ****      ****      **          ***** ");
+        System.out.println("OOPS");
     }
 }
